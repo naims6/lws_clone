1d57073a9767ee1d259047e077221d872ce12cbc
naims6 <naimdrive6@gmail.com>
2025-07-30 15:00:23 +0600

    added js in review section for opening all review

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// For Menu Opening And Closing
 const menuBtn = document.getElementById("menubtn");
 const menuIcon = document.getElementById("menuicon");
 const menuMobile = document.getElementById("mobile-menu");
@@ -10,4 +11,15 @@ menuBtn.addEventListener("click", () => {
     menuMobile.classList.add("hidden", "opacity-0");
     menuMobile.classList.remove("opacity-100");
   }
+});
+
+// For Showing All Review
+const moreComment = document.querySelector(".more-comment");
+const reviewBox = document.querySelector(".review-box");
+const reviewOverlay = document.querySelector(".review-overlay");
+console.log(reviewBox);
+moreComment.addEventListener("click", () => {
+  reviewBox.classList.add("h-full");
+  reviewBox.classList.remove("h-[900px]");
+  reviewOverlay.classList.add("hidden");
 });
